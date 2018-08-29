1c337e85a9b933a86d1471b846571a9aa7826c63
Ivanna Voloshchak <zvir44@gmail.com>
2018-08-29 18:56:12 +0300

    added filters method for date

--- src/Program.java
+++ src/Program.java
@@ -1,11 +1,14 @@
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
+
         Date birthday1 = stringToDate("2002-04-02");
-        Date birthday2 = stringToDate("2002- 7-12");//date by using method
+        Date birthday2 = stringToDate("2002- 07-12");//date by using method
 //       Date birthday3 = new Date(2001, 8, 10);// date by using java constructor
         Date birthday3 = stringToDate("2001-08-10");
 
@@ -14,6 +17,7 @@ public class Program {
         // Student st3 = new Student("Ben", "Smith", new Date(2001, 8, 10), Sex.male);// using java constructor
         Student st3 = new Student("Ben", "Smith", birthday3, Sex.male);
         StudentClass studentClass1 = new StudentClass("A", 11);
+
         studentClass1.add(st1);
         studentClass1.add(st2);
         studentClass1.add(st3);
@@ -24,16 +28,32 @@ public class Program {
         Date birthday14 = stringToDate("2002-06-11");
         StudentClass studentClass2 = new StudentClass("B", 11);
         Student st11 = new Student("Lee", "Stevenson", birthday11, Sex.male);
-        Student st12 = new Student("Molly", "Longl",birthday12, Sex.female);
-        Student st13 = new Student("Olivia", "Owen",birthday13, Sex.female);
+        Student st12 = new Student("Molly", "Longl", birthday12, Sex.female);
+        Student st13 = new Student("Olivia", "Owen", birthday13, Sex.female);
         Student st14 = new Student("David", "Zmiya", birthday14, Sex.male);
         studentClass2.add(st11);
         studentClass2.add(st12);
         studentClass2.add(st13);
         studentClass2.add(st14);
         System.out.println(studentClass2.studentList.get(1));
-    }
+        List<Student> studentList1 = new ArrayList<>();
+        for (Student student :studentClass1.studentList ) {
+           if( student.birthday.after(stringToDate("2001-11-01"))){
+               studentList1.add(student);
+               System.out.println(studentList1);
 
+            }
+        }
+        List<Student> studentList2 = new ArrayList<>();
+        for (Student student :studentClass2.studentList ) {
+            if( student.birthday.after(stringToDate("2003-01-01"))){
+                studentList2.add(student);
+                System.out.println(studentList2);
+
+            }
+        }
+
+    }
     public static Date stringToDate(String s) {
         Date date = null;
         try {
